039819fb4bff40feef476d298d7b73be18835769
schiwnn26 <codmasterd@gmail.com>
2025-03-19 20:48:02 -0400

    added ship to screen

--- main.py
+++ main.py
@@ -3,12 +3,19 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    pygame.init()
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -18,7 +25,13 @@ def main():
                 return
 
         screen.fill(000, rect=None, special_flags=0)
+
+        player.draw(screen)
+
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick()/1000
 
 if __name__ == "__main__":
     main()
